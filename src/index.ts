dfb38de4137b4c7ae20463cfbff789a55d36ac8b
arkawWebDev <arkar84848484@gmail.com>
2023-09-07 02:35:11 +0630

    any-type

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,22 @@
-// num declare
-let num: number = 10;
+let person: any;
+
+person = "arkar";
+person = 10;
+person = true;
+
+// array
+let mixed: any[];
+mixed = ["codehub", 10, true];
+
+// object
+let personOne: {
+  name: any;
+  age: any;
+  passed: any;
+};
+
+personOne = {
+  name: 123,
+  age: "20",
+  passed: "true",
+};
