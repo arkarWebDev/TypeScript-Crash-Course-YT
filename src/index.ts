f09bc2ce59f4d47c5c50dc92a92657a3dddb8a5a
arkawWebDev <arkar84848484@gmail.com>
2023-09-07 02:28:06 +0630

    explicit-types

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,40 @@
-// num declare
-let num: number = 10;
+// Explicit Types
+let person: string;
+person = "arkar";
+// person=true;
+
+let num: number;
+num = 10;
+// num = "10"
+
+let bool: boolean;
+bool = true;
+bool = false;
+// bool = "true"
+
+// array
+let workers: string[];
+// workers = ["arkar",10,true]
+
+let nums: number[];
+nums = [1, 2, 3, 4];
+
+let bools: boolean[];
+bools = [true, false];
+
+// union type
+let mixed: (boolean | string)[];
+mixed = [true, "arkar", false, "abcd"];
+
+// object
+let personOne: {
+  name: string;
+  age: number;
+  passed: boolean;
+};
+
+personOne = {
+  name: "arkar",
+  age: 20,
+  passed: true,
+};
