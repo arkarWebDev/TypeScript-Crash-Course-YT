ad6140a2a75963e40846d0e82d567effa22f2b8c
arkawWebDev <arkar84848484@gmail.com>
2023-09-07 01:38:31 +0630

    array&object

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,16 @@
 "use strict";
-let num = 10;
+let person = ["arkar", "jowel", "kyaw kyaw"];
+person.push("hla hla");
+let nums = [10, 20, 30, 40, 50];
+nums.push(60);
+let mixed = ["arkar", "jowel", 10, 20, false];
+mixed.push("kyaw kyaw");
+mixed.push(30);
+mixed.push(true);
+let team = {
+    name: "codehub",
+    passed: false,
+    age: 20,
+};
+team.name = "coding";
+console.log(team);
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,25 @@
-// num declare
-let num: number = 10;
+// array
+let person = ["arkar", "jowel", "kyaw kyaw"];
+person.push("hla hla");
+// person.push(true);
+// console.log(person);
+
+let nums = [10, 20, 30, 40, 50];
+nums.push(60);
+// nums.push("70");
+// console.log(nums);
+
+let mixed = ["arkar", "jowel", 10, 20, false];
+mixed.push("kyaw kyaw");
+mixed.push(30);
+mixed.push(true);
+// console.log(mixed);
+
+// object
+let team = {
+  name: "codehub",
+  passed: false,
+  age: 20,
+};
+
+console.log(team);
