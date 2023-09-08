f364f7c66f84d83b1be211cd6bb9c317b6f0f236
arkawWebDev <arkar84848484@gmail.com>
2023-09-09 00:59:13 +0630

    type-aliases

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,21 @@
 "use strict";
-let num = 10;
+let getInfo = (name, age) => {
+    console.log(`${name} is ${age} years old.`);
+};
+getInfo("arkar", 20);
+let getFullInfo = (info) => {
+    console.log(`${info.name} is ${info.age} years old and working at ${info.job}`);
+};
+getFullInfo({
+    name: "arkar",
+    age: 20,
+    job: "code hub",
+});
+let getFullInfomation = (info) => {
+    console.log(`${info.name} is ${info.age} years old and working at ${info.job}`);
+};
+getFullInfomation({
+    name: "arkar",
+    age: 20,
+    job: "code hub",
+});
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,29 @@
-// num declare
-let num: number = 10;
+type strOrNum = number | string;
+type infoObj = { name: string; age: strOrNum; job: string };
+
+let getInfo = (name: string, age: strOrNum): void => {
+  console.log(`${name} is ${age} years old.`);
+};
+getInfo("arkar", 20);
+
+let getFullInfo = (info: infoObj) => {
+  console.log(
+    `${info.name} is ${info.age} years old and working at ${info.job}`
+  );
+};
+getFullInfo({
+  name: "arkar",
+  age: 20,
+  job: "code hub",
+});
+
+let getFullInfomation = (info: infoObj) => {
+  console.log(
+    `${info.name} is ${info.age} years old and working at ${info.job}`
+  );
+};
+getFullInfomation({
+  name: "arkar",
+  age: 20,
+  job: "code hub",
+});
