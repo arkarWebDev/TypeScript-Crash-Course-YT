f029192123b9ad36f81dd5360506b725f30c6a51
arkawWebDev <arkar84848484@gmail.com>
2023-09-10 03:09:38 +0630

    enum

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,38 @@
 "use strict";
-let num = 10;
+let drive = (direction) => {
+    if (direction === 0) {
+        console.log("driving to up.");
+    }
+    else if (direction === 1) {
+        console.log("driving to down.");
+    }
+    else if (direction === 2) {
+        console.log("driving to left.");
+    }
+    else if (direction === 3) {
+        console.log("driving to right.");
+    }
+};
+var Direction;
+(function (Direction) {
+    Direction[Direction["Up"] = 0] = "Up";
+    Direction[Direction["Down"] = 1] = "Down";
+    Direction[Direction["Left"] = 2] = "Left";
+    Direction[Direction["Right"] = 3] = "Right";
+})(Direction || (Direction = {}));
+let eDrive = (direction) => {
+    if (direction === Direction.Up) {
+        console.log("driving to up.");
+    }
+    else if (direction === Direction.Down) {
+        console.log("driving to down.");
+    }
+    else if (direction === Direction.Left) {
+        console.log("driving to left.");
+    }
+    else if (direction === Direction.Right) {
+        console.log("driving to right.");
+    }
+};
+eDrive(Direction.Right);
+console.log(Direction.Right);
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,37 @@
-// num declare
-let num: number = 10;
+// without eums
+let drive = (direction: 0 | 1 | 2 | 3): void => {
+  if (direction === 0) {
+    console.log("driving to up.");
+  } else if (direction === 1) {
+    console.log("driving to down.");
+  } else if (direction === 2) {
+    console.log("driving to left.");
+  } else if (direction === 3) {
+    console.log("driving to right.");
+  }
+};
+
+// drive(0);
+
+// with emun
+enum Direction {
+  Up, //0
+  Down, //1
+  Left, //2
+  Right, //3
+}
+
+let eDrive = (direction: 0 | 1 | 2 | 3): void => {
+  if (direction === Direction.Up) {
+    console.log("driving to up.");
+  } else if (direction === Direction.Down) {
+    console.log("driving to down.");
+  } else if (direction === Direction.Left) {
+    console.log("driving to left.");
+  } else if (direction === Direction.Right) {
+    console.log("driving to right.");
+  }
+};
+
+eDrive(Direction.Right);
+console.log(Direction.Right);
