195d3110ad79f867a9fc1e93f54bf599878c66c0
arkawWebDev <arkar84848484@gmail.com>
2023-09-10 02:38:48 +0630

    interface

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,57 @@
-// num declare
-let num: number = 10;
+// interface
+// extend / optional property
+
+interface User {
+  name: string;
+  readonly email: string;
+  phone_number?: number;
+  //   isLogin: () => boolean;
+  isLogin(): boolean;
+  getAge(age: number): number;
+}
+
+interface User {
+  isDriving(status: boolean): string;
+}
+
+interface Editor extends User {
+  deleteUser(): string;
+}
+
+interface Admin extends User, Editor {
+  addUser(): string;
+}
+
+let userOne: User = {
+  name: "userOne",
+  email: "userOne.com",
+  isLogin: () => {
+    return true;
+  },
+  getAge() {
+    return 20;
+  },
+  isDriving() {
+    return "userOne is driving now.";
+  },
+};
+
+console.log(userOne.email);
+
+let userTwo: User = {
+  name: "userTwo",
+  email: "userTwo.com",
+  isLogin: () => {
+    return false;
+  },
+  getAge: () => {
+    return 22;
+  },
+  isDriving() {
+    return "userTwo is drived 2min ago.";
+  },
+};
+
+console.log(userTwo);
+
+// let adminOne: Admin = {};
